fe420aa8a5a123ef3426f6e611c51093e1819916
Ansharahkhan76 <anshrahmhirah@gmail.com>
2024-03-23 16:05:27 +0500

    Number guessing game in typescript

---
+++ index.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+
+type ansType = {
+    userGuess : number
+}
+
+const SystemGenerateNum = Math.floor(Math.random() * 10);
+
+const answer : ansType = await inquirer.prompt([
+    {
+        type : "number",
+        name : "userGuess",
+        message: `Please enter a number between 1 to 10: `,
+    }
+])
+
+const {userGuess} = answer;
+
+// If, else condition
+
+if (userGuess === SystemGenerateNum) {
+    console.log(userGuess, "UserGuess", SystemGenerateNum, "SYs")     // To check  what is the value of random number and user answer
+    console.log("Congratulations your answer is Correct. \n You Win!")
+}
+else {
+    console.log("Please try again, Better luck next time")
+}
+
+console.log("Random number is :",SystemGenerateNum);
